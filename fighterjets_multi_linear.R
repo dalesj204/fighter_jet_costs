eaa04a01805e314d41fc5dafd0a289457c9d43ef
Jordan Dales <jdales20.jd@gmail.com>
2023-12-06 20:18:00 -0500

    added csv data and mlr regression code

---
+++ fighterjets_multi_linear.R
@@ -0,0 +1,18 @@
+setwd("/Users/jordan.Mac/Desktop/college/clarkson/fall23/stats_project") # change this
+cat("\f") # clean the console
+rm(list = ls()) # clean the environment
+# Lets read the data
+# library(readxl)
+df <- read.csv("fighterjetcosts.csv")
+
+#df$vs <- factor(df$vs)
+# Perform multiple linear regression
+fit_mlr <- lm(FlyawayCost ~ Thrust + Climb + MTOW, data = df)
+
+res <- resid(fit_mlr)
+plot(fitted(fit_mlr), res, main = "Residual Plot for Multi-Linear Regression",
+     xlab = "Fitted", ylab = "Residual")
+
+# Print the summary of the regression model
+print(summary(fit_mlr))
+
