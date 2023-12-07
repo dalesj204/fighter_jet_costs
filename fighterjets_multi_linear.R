b0abeb56d342919c99af330a2a48ff37c303df44
Jordan Dales <jdales20.jd@gmail.com>
2023-12-06 22:58:21 -0500

    update order of code

--- fighterjets_multi_linear.R
+++ fighterjets_multi_linear.R
@@ -9,6 +9,9 @@ df <- read.csv("fighterjetcosts.csv")
 # Perform multiple linear regression
 fit_mlr <- lm(FlyawayCost ~ Thrust + Climb + MTOW, data = df)
 
+# Print the summary of the regression model
+print(summary(fit_mlr))
+
 res <- resid(fit_mlr)
 plot(fitted(fit_mlr), res, main = "Residual Plot for Multi-Linear Regression",
      xlab = "Fitted", ylab = "Residual")
@@ -17,6 +20,5 @@ abline(0,0) # horizontal line at (0,0)
 qqnorm(res) # make a Q-Q plot
 qqline(res) # add diagonal line
 
-# Print the summary of the regression model
-print(summary(fit_mlr))
+
 
