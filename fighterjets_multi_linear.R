d65e78104601a9f3e3538ef18fb6f328d5d1f760
Jordan Dales <jdales20.jd@gmail.com>
2023-12-08 10:29:19 -0500

    updated reg and graphs

--- fighterjets_multi_linear.R
+++ fighterjets_multi_linear.R
@@ -7,7 +7,7 @@ df <- read.csv("fighterjetcosts.csv")
 
 #df$vs <- factor(df$vs)
 # multiple linear regression
-fit_mlr <- lm(FlyawayCost ~ Thrust + Climb + MTOW, data = df)
+fit_mlr <- lm(FlyawayCost ~ Thrust + Climb + MTOW + FFD, data = df)
 
 # print the summary of the regression model
 print(summary(fit_mlr))
@@ -17,13 +17,14 @@ fit_mlr2 <- lm(FlyawayCost ~ Thrust + FFD, data = df)
 print(summary(fit_mlr2))
 
 
+# extras
 res <- resid(fit_mlr)
 plot(fitted(fit_mlr), res, main = "Residual Plot for Multiple Linear Regression",
      xlab = "Fitted", ylab = "Residual")
-abline(0,0) # horizontal line at (0,0)
+abline(0,0, col = "red") # horizontal line at (0,0)
 
 qqnorm(res) # make a Q-Q plot
-qqline(res) # add diagonal line
+qqline(res, col = "red") # add diagonal line
 
 plot(density(res), main="Density Plot") # density plot to verify ~N
 
